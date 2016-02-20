b27cd713a82e7597dcef0da530353729ee108791
Toshiaki Maki <makingx@gmail.com>
2016-02-19 23:37:07 -0800

    Fix

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-ln -s `pwd`/.emacs.d/init.el $HOME/.emacs.d
+if [ ! -d $HOME/.emacs.d ];then
+    echo "Create $HOME/.emacs.d"
+    mkdir -p $HOME/.emacs.d
+fi
+
+if [ -f $HOME/.emacs.d/install.sh ];then
+    echo "Remove $HOME/.emacs.d/init.el"
+    rm -f $HOME/.emacs.d/init.el
+fi
+
+if [ ! -h $HOME/.emacs.d/init.el ];then
+    echo "Link $HOME/.emacs.d/init.el"
+    ln -s `pwd`/.emacs.d/init.el $HOME/.emacs.d
+fi
